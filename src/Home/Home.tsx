ece175e0c08817abee3fb9e7b426e8b4411e7651
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-07-25 21:48:24 -0300

    Create Component Button and styled with styled-components

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import styles from './Home.module.css';
+import { Button } from '../Components/Button/Button';
 
 export function Home(){
   return (
     <div>
-
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='success'/>
+      <Button variant='danger'/>
+      <Button/>
     </div>
   );
 }
