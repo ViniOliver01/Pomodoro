45f0315ec9c4193bb7e804fea5a2e8eb6d8abf2f
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-07-26 19:31:11 -0300

    Create theme style and type for it

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components'
+import { defaultTheme } from '../Styles/Themes/Default'
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType{}
+}
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,13 +1,19 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from '../Components/Button/Button';
+
+import { defaultTheme } from './../Styles/Themes/Default';
+import { GlobalStyle } from './../Styles/Themes/Global';
 
 export function Home(){
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary'/>
       <Button variant='secondary'/>
       <Button variant='success'/>
       <Button variant='danger'/>
       <Button/>
-    </div>
+
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
